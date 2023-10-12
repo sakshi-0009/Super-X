d98d7ad098f255234798f6134f1746905514e1e9
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-12 19:07:17 +0530

    Number on it's left less than or equal to itself

--- Assignment_01/GreaterNum.java
+++ Assignment_01/GreaterNum.java
@@ -1,12 +1,20 @@
 //WAP to find number which has number on it's left is less than or equal to itself.
-//Incomplete
 
 package Assignment_01;
 
 public class GreaterNum {
     public static void main(String[] args) {
-    	int input = 456975962;
+    	int n = 456975962;
         int max = Integer.MIN_VALUE;
+        while(n!=0) {
+        	int rem = n%10;
+        	if(max<rem) {
+        		max = rem;
+        	}
+        	n = n/10;
+        }
+        System.out.println(max);
     }
 }
-
+//Time complexity : O(N)
+//Space complecxity : O(1)
