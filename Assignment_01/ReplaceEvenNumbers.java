b7f3b0835074bc10abc060921d214f2cbd14a58e
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-12 18:42:26 +0530

    Replace even numbers with 1

--- Assignment_01/ReplaceEvenNumbers.java
+++ Assignment_01/ReplaceEvenNumbers.java
@@ -1,33 +1,33 @@
+//WAP to reverse number and put it on successive number sum into an array :
+
 package Assignment_01;
-
 import java.io.*;
-
 public class ReplaceEvenNumbers {
     public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.print("Enter the size of the array: ");
+        System.out.print("Enter size of an array: ");
         int size = Integer.parseInt(br.readLine());
 
-        int[] firstArray = new int[size];
-        int[] secondArray = new int[size];
+        int[] arr1 = new int[size];
+        int[] arr2 = new int[size];
 
-        System.out.println("Enter the elements of the first array:");
-        for (int i = 0; i < size; i++) {
-            firstArray[i] = Integer.parseInt(br.readLine());
-            if (firstArray[i] % 2 == 0) {
-                secondArray[i] = 1;
+        System.out.println("Enter elements of first array:");
+        for (int i=0; i< size; i++){
+        	arr1[i] = Integer.parseInt(br.readLine());
+            if (arr1[i] % 2 == 0){
+            	arr2[i] = 1;
             }
         }
-        System.out.println("First Array:");
-        for (int i = 0; i < size; i++) {
-            System.out.print(firstArray[i] + " ");
+        System.out.println("Array 1:");
+        for (int i=0; i< size; i++) {
+            System.out.print(arr1[i]+" ");
         }
         System.out.println();
-
-        System.out.println("Second Array:");
-        for (int i = 0; i < size; i++) {
-            System.out.print(secondArray[i] + " ");
+        System.out.println("Array 2:");
+        for (int i =0; i< size; i++){
+            System.out.print(arr2[i]+" ");
         }
     }
 }
+//Time complexity : O(N)
+//Space complexity : O(N)
