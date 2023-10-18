ba5487631df1edd2296d2893f4274f0a08cdb452
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-18 07:12:55 +0530

    Odd and Even numbers

---
+++ Assignment_03/OddEven.java
@@ -0,0 +1,15 @@
+package Assignment_03;
+import java.io.*;
+public class OddEven {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter number : ");
+		double n = Double.parseDouble(br.readLine());
+		if(n%2==0) {
+			System.out.println(n+" is even number.");
+		}else {
+			System.out.println(n+" is odd number.");
+		}
+	}
+}
