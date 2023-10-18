ec57680c3acd1b97375ca80b7b861e357e1dc503
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-18 07:18:07 +0530

    Odd and Even numbers in given range

---
+++ Assignment_03/OddEvenInRange.java
@@ -0,0 +1,17 @@
+package Assignment_03;
+import java.io.*;
+public class OddEvenInRange {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter Start : ");
+		int start = Integer.parseInt(br.readLine());
+		System.out.println("Enter End : ");
+		int end = Integer.parseInt(br.readLine());
+		for(int i=start; i<=end; i++) {
+			if(i%2==1) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
