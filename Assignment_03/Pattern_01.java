f4b9022eb477cb6fde07f9012cd2155bdb16ae5f
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-18 07:04:58 +0530

    Pattern-1

---
+++ Assignment_03/Pattern_01.java
@@ -0,0 +1,16 @@
+package Assignment_03;
+import java.io.*;
+public class Pattern_01 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter rows : ");
+		int rows = Integer.parseInt(br.readLine());
+		for(int i=1; i<=rows; i++) {
+			for(int j=1; j<=rows; j++) {
+				System.out.print((i+j-1)+" ");
+			}
+			System.out.println();
+		}
+	}
+}
