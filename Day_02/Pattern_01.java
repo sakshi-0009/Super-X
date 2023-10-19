2a48e8377423700d7eab82f5fc3ec179f6f9ef6d
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-19 22:32:29 +0530

    Pattern-01

--- Day_02/Pattern_01.java
+++ Day_02/Pattern_01.java
@@ -1,5 +1,24 @@
 package Day_02;
 
+import java.io.*;
+
 public class Pattern_01 {
 
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter rows : ");
+		int rows = Integer.parseInt(br.readLine());
+		
+		char ch = 'A';
+		char ch1 = ch;
+		for (int i = 1; i <= rows; i++) {
+			ch = ch1;
+            for (int j = 1; j <= rows; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            ch1++;
+            System.out.println();
+        }
+	}
 }
