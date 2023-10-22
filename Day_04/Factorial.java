d60776bce9a9769dc264af116f61afde23b4232a
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-22 22:11:19 +0530

    Factorial of given number

--- Day_04/Factorial.java
+++ Day_04/Factorial.java
@@ -2,14 +2,17 @@ package Day_04;
 import java.io.*;
 public class Factorial {
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter number : ");
-		int num = Integer.parseInt(br.readLine());
+	static void factorial(int num) {
 		int prod = 1;
 		for(int i=1; i<=num; i++) {
 			prod = prod*i;
 		}
 		System.out.println("Factorial of "+num+" is "+prod);
+	}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter number : ");
+		int num = Integer.parseInt(br.readLine());
+		factorial(num);
 	}
 }
---
+++ Day_04/SumOfDigits.java
@@ -0,0 +1,5 @@
+package Day_04;
+
+public class SumOfDigits {
+
+}
