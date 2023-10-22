79a9a3a7137753e1adc9b9c27c730f026faae33b
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-22 22:18:38 +0530

    Sum of all numbers in given range

--- Day_04/SumOfDigits.java
+++ Day_04/SumOfDigits.java
@@ -1,5 +1,21 @@
 package Day_04;
-
+import java.io.*;
 public class SumOfDigits {
 
+	static void Sum(int start, int end) {
+		int sum = 0;
+		for(int i=start; i<=end; i++) {
+			sum = sum+i;
+		}
+		System.out.println("Sum of all numbers in given range is: "+sum);
+
+	}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter start : ");
+		int start = Integer.parseInt(br.readLine());
+		System.out.println("Enter end : ");
+		int end = Integer.parseInt(br.readLine());
+		Sum(start,end);
+	}
 }
